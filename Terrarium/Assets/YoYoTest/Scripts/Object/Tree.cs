baa6d12476024db7166663450d1638ed28d860c4
TOTHEMOON\youdo <1023571809@qq.com>
2025-08-05 20:51:50 +0800

    三种植物！

---
+++ Terrarium/Assets/YoYoTest/Scripts/Object/Tree.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour, IGetObjectClass, IGetQuantityLimits
+{
+    public string BigClass => "Plant";
+
+    public string SmallClass => "Tree";
+
+    public int quantityLimits = 10;
+    public int QuantityLimits => quantityLimits;
+
+    private float growthSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Events.OnCreateObject.Invoke(this);
+
+        // 检测地面并设置生长速度
+        GroundDetectionUtils.SetGrowthSpeedFromGroundSimple(transform.position, (fertility) => growthSpeed = growthSpeed * fertility);
+
+        // 设置生长速度
+        GetComponent<IGrowth>().GrowthSpeed = growthSpeed;
+        GetComponent<IGrowth>().Growth();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        // Events.OnSelectPrefab.AddListener(OnSelectPrefab);
+    }
+
+
+    private void OnDestroy()
+    {
+        Events.OnDestroyObject.Invoke(this);
+    }
+
+
+
+}
