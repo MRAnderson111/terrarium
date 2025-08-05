aa6b2a648779764044008da2e783f7dd26b07bd2
TOTHEMOON\youdo <1023571809@qq.com>
2025-08-05 22:57:31 +0800

    蚂蚁生成

--- Terrarium/Assets/YoYoTest/Scripts/UI/SelectButton.cs
+++ Terrarium/Assets/YoYoTest/Scripts/UI/SelectButton.cs
@@ -7,12 +7,13 @@ public class SelectButton : MonoBehaviour
 {
     public GameObject selectPrefab;
     private Button button;
-    public GameObject selectPosition;
+    private GameObject selectPosition;
     // Start is called before the first frame update
     void Start()
     {
         button = GetComponent<Button>();
         button.onClick.AddListener(OnClick);
+        selectPosition =  Resources.Load<GameObject>("Prefabs/SelectPosition");
     }
 
     // Update is called once per frame
