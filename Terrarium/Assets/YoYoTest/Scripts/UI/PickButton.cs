12e1c08ad2d4daf17cce44a9691c492bb2f571b3
TOTHEMOON\youdo <1023571809@qq.com>
2025-08-10 16:53:35 +0800

    准备选数据

---
+++ Terrarium/Assets/YoYoTest/Scripts/UI/PickButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickButton : MonoBehaviour
+{
+    public GameObject selectPrefab;
+    public PickPrefabUI PickPrefabUI;
+    private Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PickPrefabUI = FindObjectOfType<PickPrefabUI>();
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClick);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("选择预制体：" + selectPrefab.name);
+        PickPrefabUI.AddPrefabToJson();
+    }
+}
