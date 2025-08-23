dd0d3144391fdf8cff6466763e5acabcd2980c3b
TOTHEMOON\youdo <1023571809@qq.com>
2025-08-23 14:04:18 +0800

    设置移动脚本

---
+++ Terrarium/Assets/YoYoTest/Scripts/NewAnt/AnimalNavMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AnimalNavMove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.destination = hit.point;
+            }
+        }
+    }
+
+    public void SetTarget(Vector3 target)
+    {
+        agent.destination = target;
+    }
+}
