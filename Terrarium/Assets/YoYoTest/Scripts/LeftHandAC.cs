45fed12cb2278b72564571b6c86467a50bc6f6f2
MRAnderson111 <1296782776@qq.com>
2025-11-09 19:53:26 +0800

    Stashed

--- Terrarium/Assets/YoYoTest/Scripts/LeftHandAC.cs
+++ Terrarium/Assets/YoYoTest/Scripts/LeftHandAC.cs
@@ -1,18 +1,60 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class LeftHandAC : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+    private InputDevice leftController;
+    private bool triggerPressed = false;
+    private bool previousTriggerState = false;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!leftController.isValid)
+        {
+            leftController = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+        }
+
+        if (leftController.isValid)
+        {
+            // 尝试多种trigger检测方式
+            bool triggerButton = false;
+            float triggerValue = 0f;
+
+            leftController.TryGetFeatureValue(CommonUsages.triggerButton, out triggerButton);
+            leftController.TryGetFeatureValue(CommonUsages.trigger, out triggerValue);
+
+            // 使用trigger按钮或trigger值大于0.5作为触发条件
+            triggerPressed = triggerButton || triggerValue > 0.5f;
+
+            // 每次按下扳机时触发动画
+            if (triggerPressed && !previousTriggerState)
+            {
+                if (animator != null)
+                {
+                    // 触发动画播放
+                    animator.SetTrigger("bIsTrigger");
+                    Debug.Log("LeftHandAC: 扳机按下 - 触发动画");
+                }
+            }
+            previousTriggerState = triggerPressed;
+        }
+
+        // 键盘测试（无论控制器是否有效都可以使用）
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (animator != null)
+            {
+                animator.SetTrigger("bIsTrigger");
+                Debug.Log("LeftHandAC: 键盘测试 - 触发动画");
+            }
+        }
     }
 }
